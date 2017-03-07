b820a747423f42dfdc5320dfb35c2353efc8e559
F. Javier R. Donado <fjrd84@gmail.com>
2017-03-07 22:51:49 +0100

    Socket io example.

--- README.md
+++ README.md
@@ -3,4 +3,13 @@
 Backend repository for the health-nlp project.
 
 ## Install Dependencies
+
 `sudo pip install Flask`
+`sudo pip install flask-socketio`
+`sudo pip install eventlet`
+
+## Start the Server
+
+After installing the dependencies, simply run:
+
+`python server.py`
--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
 
 app.debug = True
 
@@ -9,4 +14,13 @@ def base_route():
     return 'health-nlp-backend'
 
 if __name__ == '__main__':
-    app.run()
+    """app.run()"""
+    socketio.run(app)
+
+@socketio.on('message')
+def handle_message(message):
+    send(message)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
